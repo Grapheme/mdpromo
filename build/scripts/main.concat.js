8cefc5ff00d4084e4e8ddecba979f33fd5fcd1a1
KostyaD <thedamaxstudio@gmail.com>
2015-07-13 10:23:43 +0300

    promo

--- build/scripts/main.concat.js
+++ build/scripts/main.concat.js
@@ -25,8 +25,13 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 !function(a){"use strict";"function"==typeof define&&define.amd?define(["jquery"],a):a(jQuery)}(function(a){"use strict";function b(a){if(a instanceof Date)return a;if(String(a).match(g))return String(a).match(/^[0-9]*$/)&&(a=Number(a)),String(a).match(/\-/)&&(a=String(a).replace(/\-/g,"/")),new Date(a);throw new Error("Couldn't cast `"+a+"` to a date object.")}function c(a){return function(b){var c=b.match(/%(-|!)?[A-Z]{1}(:[^;]+;)?/gi);if(c)for(var e=0,f=c.length;f>e;++e){var g=c[e].match(/%(-|!)?([a-zA-Z]{1})(:[^;]+;)?/),i=new RegExp(g[0]),j=g[1]||"",k=g[3]||"",l=null;g=g[2],h.hasOwnProperty(g)&&(l=h[g],l=Number(a[l])),null!==l&&("!"===j&&(l=d(k,l)),""===j&&10>l&&(l="0"+l.toString()),b=b.replace(i,l.toString()))}return b=b.replace(/%%/,"%")}}function d(a,b){var c="s",d="";return a&&(a=a.replace(/(:|;|\s)/gi,"").split(/\,/),1===a.length?c=a[0]:(d=a[0],c=a[1])),1===Math.abs(b)?d:c}var e=100,f=[],g=[];g.push(/^[0-9]*$/.source),g.push(/([0-9]{1,2}\/){2}[0-9]{4}( [0-9]{1,2}(:[0-9]{2}){2})?/.source),g.push(/[0-9]{4}([\/\-][0-9]{1,2}){2}( [0-9]{1,2}(:[0-9]{2}){2})?/.source),g=new RegExp(g.join("|"));var h={Y:"years",m:"months",w:"weeks",d:"days",D:"totalDays",H:"hours",M:"minutes",S:"seconds"},i=function(b,c,d){this.el=b,this.$el=a(b),this.interval=null,this.offset={},this.instanceNumber=f.length,f.push(this),this.$el.data("countdown-instance",this.instanceNumber),d&&(this.$el.on("update.countdown",d),this.$el.on("stoped.countdown",d),this.$el.on("finish.countdown",d)),this.setFinalDate(c),this.start()};a.extend(i.prototype,{start:function(){null!==this.interval&&clearInterval(this.interval);var a=this;this.update(),this.interval=setInterval(function(){a.update.call(a)},e)},stop:function(){clearInterval(this.interval),this.interval=null,this.dispatchEvent("stoped")},pause:function(){this.stop.call(this)},resume:function(){this.start.call(this)},remove:function(){this.stop(),f[this.instanceNumber]=null,delete this.$el.data().countdownInstance},setFinalDate:function(a){this.finalDate=b(a)},update:function(){return 0===this.$el.closest("html").length?void this.remove():(this.totalSecsLeft=this.finalDate.getTime()-(new Date).getTime(),this.totalSecsLeft=Math.ceil(this.totalSecsLeft/1e3),this.totalSecsLeft=this.totalSecsLeft<0?0:this.totalSecsLeft,this.offset={seconds:this.totalSecsLeft%60,minutes:Math.floor(this.totalSecsLeft/60)%60,hours:Math.floor(this.totalSecsLeft/60/60)%24,days:Math.floor(this.totalSecsLeft/60/60/24)%7,totalDays:Math.floor(this.totalSecsLeft/60/60/24),weeks:Math.floor(this.totalSecsLeft/60/60/24/7),months:Math.floor(this.totalSecsLeft/60/60/24/30),years:Math.floor(this.totalSecsLeft/60/60/24/365)},void(0===this.totalSecsLeft?(this.stop(),this.dispatchEvent("finish")):this.dispatchEvent("update")))},dispatchEvent:function(b){var d=a.Event(b+".countdown");d.finalDate=this.finalDate,d.offset=a.extend({},this.offset),d.strftime=c(this.offset),this.$el.trigger(d)}}),a.fn.countdown=function(){var b=Array.prototype.slice.call(arguments,0);return this.each(function(){var c=a(this).data("countdown-instance");if(void 0!==c){var d=f[c],e=b[0];i.prototype.hasOwnProperty(e)?d[e].apply(d,b.slice(1)):null===String(e).match(/^[$A-Z_][0-9A-Z_$]*$/i)?(d.setFinalDate.call(d,e),d.start()):a.error("Method %s does not exist on jQuery.countdown".replace(/\%s/gi,e))}else new i(this,b[0],b[1])})}});
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
 window.vkw = function() {
-	VK.Widgets.Group("vk_groups", {mode: 0, width: "358", height: "389", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 20003922);
+	VK.Widgets.Group("vk_groups", {mode: 0, width: "358", height: "389", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 33380340);
 }
 window.countdown = function() {
 	$(".js-countdown")
@@ -36,8 +41,66 @@ window.countdown = function() {
      );
    });
 }
+window.scrollShow = function() {
+	var addActive = function(elem, eq, max, time) {
+		elem.filter('[data-number="' + eq + '"]').addClass('active');
+		eq++;
+		if(eq < max) {
+			setTimeout(function(){
+				addActive(elem, eq, max, time);
+			}, time)
+		}
+	}
+	var show = function() {
+		$('[data-row]').each(function(){
+			var this_row = $(this).attr('data-row');
+			var this_elems = $('[data-row="' + this_row + '"]');
+			var this_length = this_elems.length;
+			if($(this).offset().top < $(window).scrollTop() + $(window).height()) {
+				addActive(this_elems, 0, this_length, 500);
+			}
+		});
+	}
+	show();
+	$(window).on('scroll', show)
+}
+window.mail = function() {
+	$('.js-mail').on('submit', function(e){
+		e.preventDefault();
+		var $this = $(this);
+		var $btn = $(this).find('[type="submit"]');
+		if(!validateEmail($this.find('input').val())) {
+			$this.find('input').addClass('error-input').trigger('focus');
+			return;
+		} else {
+			$this.find('input').removeClass('error-input');
+		}
+		$btn.addClass('loading');
+		$.ajax({
+			url: $this.attr('action'),
+			method: $this.attr('method'),
+			data: $this.serialize(),
+			dataType: 'json'
+		}).done(function(data){
+			$('.js-form-parent').slideUp(function(){
+				$('.js-mail-success').slideDown();
+			});
+			setTimeout(function(){
+				$('.js-form-parent').parent().slideUp();
+			}, 2000);
+		}).fail(function(data){
+			console.log(data);
+			$('.js-mail-fail').slideDown();
+		}).always(function(){
+			$btn.removeClass('loading');
+		});
+		return false;
+	});
+}
 
 $(function(){
-	vkw();
+	scrollShow();
 	countdown();
-})
+	mail();
+	vkw();
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
+function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
 window.vkw = function() {
-	VK.Widgets.Group("vk_groups", {mode: 0, width: "358", height: "389", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 20003922);
+	VK.Widgets.Group("vk_groups", {mode: 0, width: "358", height: "389", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 33380340);
 }
 window.countdown = function() {
 	$(".js-countdown")
@@ -9,8 +14,66 @@ window.countdown = function() {
      );
    });
 }
+window.scrollShow = function() {
+	var addActive = function(elem, eq, max, time) {
+		elem.filter('[data-number="' + eq + '"]').addClass('active');
+		eq++;
+		if(eq < max) {
+			setTimeout(function(){
+				addActive(elem, eq, max, time);
+			}, time)
+		}
+	}
+	var show = function() {
+		$('[data-row]').each(function(){
+			var this_row = $(this).attr('data-row');
+			var this_elems = $('[data-row="' + this_row + '"]');
+			var this_length = this_elems.length;
+			if($(this).offset().top < $(window).scrollTop() + $(window).height()) {
+				addActive(this_elems, 0, this_length, 500);
+			}
+		});
+	}
+	show();
+	$(window).on('scroll', show)
+}
+window.mail = function() {
+	$('.js-mail').on('submit', function(e){
+		e.preventDefault();
+		var $this = $(this);
+		var $btn = $(this).find('[type="submit"]');
+		if(!validateEmail($this.find('input').val())) {
+			$this.find('input').addClass('error-input').trigger('focus');
+			return;
+		} else {
+			$this.find('input').removeClass('error-input');
+		}
+		$btn.addClass('loading');
+		$.ajax({
+			url: $this.attr('action'),
+			method: $this.attr('method'),
+			data: $this.serialize(),
+			dataType: 'json'
+		}).done(function(data){
+			$('.js-form-parent').slideUp(function(){
+				$('.js-mail-success').slideDown();
+			});
+			setTimeout(function(){
+				$('.js-form-parent').parent().slideUp();
+			}, 2000);
+		}).fail(function(data){
+			console.log(data);
+			$('.js-mail-fail').slideDown();
+		}).always(function(){
+			$btn.removeClass('loading');
+		});
+		return false;
+	});
+}
 
 $(function(){
+	scrollShow();
 	countdown();
+	mail();
 	vkw();
 });
